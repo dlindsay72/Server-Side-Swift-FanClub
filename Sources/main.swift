69c3b678c19672590f06397355a5b2daac1f99f6
Dan Lindsay <dlindsay72@hotmail.com>
2017-03-04 13:57:54 -0500

    setup home.stencil, with dummy info for username. Created functions for context which holds dummy username, and send which shows an error. Configured router to get our basic forum template

--- Sources/main.swift
+++ Sources/main.swift
@@ -9,6 +9,16 @@ import KituraStencil
 import LoggerAPI
 import SwiftyJSON
 
+func send(error: String, code: HTTPStatusCode, to response: RouterResponse) {
+  _ = try? response.status(code).send(error).end()
+}
+
+func context(for request: RouterRequest) -> [String: Any] {
+  var result = [String: String]()
+  result["username"] = "testing"
+  return result
+}
+
 HeliumLogger.use()
 
 let connectionProperties = ConnectionProperties(host: "localhost", port: 5984, secured: false)
@@ -19,6 +29,25 @@ let router = Router()
 router.setDefault(templateEngine: StencilTemplateEngine())
 router.post("/", middleware: BodyParser())
 router.all("/static", middleware: StaticFileServer())
+
+router.get("/") {
+  request, response, next in
+
+  database.queryByView("forums", ofDesign: "forum", usingParameters: []) { forums, error in
+    defer { next() }
+
+    if let error = error {
+      //something went wrong
+      send(error: error.localizedDescription, code: .internalServerError, to: response)
+    } else if let forums = forums {
+      //success!
+      var forumContext = context(for: request)
+      forumContext["forums"] = forums["rows"].arrayObject
+
+      _ = try? response.render("home", context: forumContext)
+    }
+  }
+}
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
 Kitura.run()
