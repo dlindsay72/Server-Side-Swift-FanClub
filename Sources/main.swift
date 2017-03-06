6a02c991328d5bdd293f604a7c9351866aa182d2
Dan Lindsay <dlindsay72@hotmail.com>
2017-03-06 15:28:43 -0500

    made stencil for users-create and users-login, can now create new users and login with the newly created user. Once logged in, username is displayed

--- Sources/main.swift
+++ Sources/main.swift
@@ -10,13 +10,43 @@ import LoggerAPI
 import SwiftyJSON
 import Stencil
 
+extension String {
+  func removingHTMLEncoding() -> String {
+    let result = self.replacingOccurrences(of: "+", with: "")
+    return result.removingPercentEncoding ?? result
+  }
+}
+
+func getPost(for request: RouterRequest, fields: [String]) -> [String: String]? {
+  guard let values = request.body else { return nil }
+  guard case .urlEncoded(let body) = values else { return nil }
+
+  var result = [String: String]()
+
+  for field in fields {
+    if let value = body[field]?.trimmingCharacters(in: .whitespacesAndNewlines) {
+      if value.characters.count > 0 {
+        result[field] = value.removingHTMLEncoding()
+        continue
+      }
+    }
+    return nil
+  }
+  return result
+}
+
 func send(error: String, code: HTTPStatusCode, to response: RouterResponse) {
   _ = try? response.status(code).send(error).end()
 }
 
+func password(from str: String, salt: String) -> String {
+  let key = PBKDF.deriveKey(fromPassword: str, salt: salt, prf: .sha512, rounds: 250_000, derivedKeyLength: 64)
+  return CryptoUtils.hexString(from: key)
+}
+
 func context(for request: RouterRequest) -> [String: Any] {
   var result = [String: String]()
-  result["username"] = "testing"
+  result["username"] = request.session?["username"].string
   return result
 }
 
@@ -46,6 +76,7 @@ namespace.registerFilter("format_date") { (value: Any?) in
 router.setDefault(templateEngine: StencilTemplateEngine(namespace: namespace))
 router.post("/", middleware: BodyParser())
 router.all("/static", middleware: StaticFileServer())
+router.all(middleware: Session(secret: "The rain in Spain falls mainly on the Spaniards"))
 
 router.get("/") {
   request, response, next in
@@ -135,10 +166,114 @@ router.get("/forum/:forumid/:messageid") {
   }
 }
 
+router.get("/users/login") {
+  request, response, next in
+  defer { next() }
+
+  try response.render("users-login", context: [:])
+}
+
+router.get("/users/create") {
+  request, response, next in
+  defer { next() }
+
+  try response.render("users-create", context: [:])
+}
+
+router.post("users/login") {
+  request, response, next in
+
+  //ensure all the correct fields are present
+  if let fields = getPost(for: request, fields: ["username", "password"]) {
+    //load the user from CouchDBClient
+    database.retrieve(fields["username"]!) { doc, error in
+      defer { next() }
+
+      if let error = error {
+        //this user doesn't exist!
+        send(error: "Unable to load user.", code: .badRequest, to: response)
+      } else if let doc = doc {
+        //load the salt and password from the document
+        let savedSalt = doc["salt"].stringValue
+        let savedPassword = doc["password"].stringValue
+
+        //hash the user's input password with the saved salt; this should produce the same password we saved
+        let testPassword = password(from: fields["password"]!, salt: savedSalt)
+
+        if testPassword == savedPassword {
+          // the pasword was correct - save the username in the sesssion and redirect to the homepage
+          request.session!["username"].string = doc["_id"].string
+          _ = try? response.redirect("/")
+        } else {
+          // wrong password!
+          print("No match")
+        }
+      }
+    }
+  } else {
+    // the form was not filled in fully
+    send(error: "Missing required fields", code: .badRequest, to: response)
+  }
+}
+
+router.post("/users/create") {
+  request, response, next in
+  defer { next() }
+
+  guard let fields = getPost(for: request, fields: ["username", "password"]) else {
+    send(error: "Missing required fields", code: .badRequest, to: response)
+    return
+  }
+
+  database.retrieve(fields["username"]!) { doc, error in
+    if let error = error {
+      // username does not exist!
+      var newUser = [String: String]()
+
+      //force the CouchDB ID to be the username
+      newUser["_id"] = fields["username"]!
+
+      // add a "type" property so our views can filter correctly
+      newUser["type"] = "user"
+
+      let saltString: String
+
+      //create a salt using one of two methods
+      if let salt = try? Random.generate(byteCount: 64) {
+        saltString = CryptoUtils.hexString(from: salt)
+      } else {
+        // this in theory ought never be used - it's an emergency fallback!
+        saltString = (fields["username"]! + fields["password"]! + "project4").digest(using: .sha512)
+      }
+      // we need to store the salt in the database so we can re-hash the password at login time
+      newUser["salt"] = saltString
+
+      //calculate the password hash for this user
+      newUser["password"] = password(from: fields["password"]!, salt: saltString)
+
+      let newUserJSON = JSON(newUser)
+
+      //send it off to CouchDB
+      database.create(newUserJSON) { id, revision, doc, error in
+        defer { next() }
+
+        if let doc = doc {
+          //send back a message that everything worked
+          response.send("OK!")
+        } else {
+          //error
+          send(error: "User could not be created", code: .internalServerError, to: response)
+        }
+      }
+    } else {
+      // username exists already!
+      send(error: "User already exists", code: .badRequest, to: response)
+    }
+  }
+}
+
 
 Kitura.addHTTPServer(onPort: 8090, with: router)
-
-
 
 
 
